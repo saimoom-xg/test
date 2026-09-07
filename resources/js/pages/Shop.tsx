646bd312ff3a8f77cc4a0dd5b98e1bfefd97e7b3
saimoom-xg <saimoom.xgenious@gmail.com>
2026-09-07 04:11:57 +0000

    Enable deployment workflow

--- resources/js/pages/Shop.tsx
+++ resources/js/pages/Shop.tsx
@@ -678,7 +678,7 @@ export default function Shop({
                         {products.data.length > 0 ? (
                             <>
                                 {/* Product Cards Grid */}
-                                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
+                                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-5 gap-6">
                                     {products.data.map((product) => {
                                         const isAdding = addingId === product.id;
                                         const mainImage = getProductImage(product);
